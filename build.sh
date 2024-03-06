9d37a2291a65294a8992ee2103762a7eeb813531
Jacob McSwain <jacob.a.mcswain@gmail.com>
2024-03-06 03:25:39 -0600

    build: cache the gallery

--- build.sh
+++ build.sh
@@ -6,11 +6,15 @@ rm -rf ./public ./content/gallery ./static/gallery
 mkdir -p ./content/gallery/my-data
 mkdir -p ./content/gallery/other-data
 mkdir -p ./static/gallery
+mkdir -p .build-cache
 
 # renovate: datasource=github-releases depName=USA-RedDragon/astro.garden-images
 GALLERY_VERSION=11
 
-curl -fSsL https://github.com/USA-RedDragon/astro.garden-images/releases/download/${GALLERY_VERSION}/gallery.tar.gz | tar -xz -C ./static/gallery
+if [ ! -f .build-cache/gallery-${GALLERY_VERSION}.tar.gz ]; then
+  curl -fSsL https://github.com/USA-RedDragon/astro.garden-images/releases/download/${GALLERY_VERSION}/gallery.tar.gz -o .build-cache/gallery-${GALLERY_VERSION}.tar.gz
+fi
+tar -xzf .build-cache/gallery-${GALLERY_VERSION}.tar.gz -C ./static/gallery
 rm -rf ./static/gallery/my-data ./static/gallery/other-data ./static/gallery/my-data.json ./static/gallery/other-data.json
 
 # We have a clean gallery directory
