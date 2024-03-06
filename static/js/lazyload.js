55d9a97fc2734f38ace6887f55b2ad28cb40b441
Jacob McSwain <jacob.a.mcswain@gmail.com>
2024-03-06 03:09:12 -0600

    clean up lazy loading

--- static/js/lazyload.js
+++ static/js/lazyload.js
@@ -3,21 +3,13 @@ const images = document.querySelectorAll("img[data-src]");
 const options = {
   root: null,
   rootMargin: "0px",
-  threshold: 0.00001
+  threshold: 0
 };
 
 let observer = new IntersectionObserver((entries, observer) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       let lazyImage = entry.target;
-      lazyImage.addEventListener('load', () => {
-        setTimeout(() => {
-          const div = lazyImage.closest('div');
-          if (div) {
-            div.classList.add('loaded');
-          }
-        }, 100);
-      });
       lazyImage.src = lazyImage.dataset.src;
       observer.unobserve(lazyImage);
     }
